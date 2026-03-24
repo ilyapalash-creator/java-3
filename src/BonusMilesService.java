526550ebf62de2300832542eb736c70d881272b0
Ilya Palash <rudut.ilya@ya.ru>
2026-03-24 13:32:55 +0300

    Correction

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-public int calculate (int price){
-    int miles;
-    miles = price / 20;
-    return miles;
-}
+    public int calculate(int price) {
+        int miles;
+        miles = price / 20;
+        return miles;
+    }
 }
